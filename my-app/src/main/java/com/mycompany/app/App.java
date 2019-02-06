a832fed44545d654411851f62e1f9c575133b144
Kishore <kishorekunal221@gmail.com>
2019-02-06 21:46:27 +0530

    maven

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,59 +1,14 @@
-/**
- * Hello world!
- *
- */
 package com.mycompany.app;
-import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
     	double num1, num2;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number:");
+        num1 = Double.parseDouble(args[0]);
+        num2 = Double.parseDouble(args[1]);
 
-        /* We are using data type double so that user
-         * can enter integer as well as floating point
-         * value
-         */
-        num1 = scanner.nextDouble();
-        System.out.print("Enter second number:");
-        num2 = scanner.nextDouble();
-
-        System.out.print("Enter an operator (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
-
-        scanner.close();
-        double output;
-
-        switch(operator)
-        {
-            case '+':
-            	output = num1 + num2;
-                break;
-
-            case '-':
-            	output = num1 - num2;
-                break;
-
-            case '*':
-            	output = num1 * num2;
-                break;
-
-            case '/':
-            	output = num1 / num2;
-                break;
-
-            /* If user enters any other operator or char apart from
-             * +, -, * and /, then display an error message to user
-             * 
-             */
-            default:
-                System.out.printf("You have entered wrong operator");
-                return;
-        }
-
-        System.out.println(num1+" "+operator+" "+num2+": "+output);
+	double output = num1+num2;
+        System.out.println(num1+" + "+num2+": "+output);
     }
 }
